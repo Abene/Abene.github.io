b9e88f13d05fe9ede191467c799169489e682f99
abene <mailabene@gmail.com>
2014-07-25 11:15:04 +0000

    added easyxdn with KG

--- js/XDMexternal.js
+++ js/XDMexternal.js
@@ -1,46 +1,56 @@
 (function(){
-    $(document).ready(function(){
+    // $(document).ready(function(){
+        $(function(){
 
-//outer javascript
-        cheetahWidget2012_IframeHeight: function(apikey,buttonPosition,hostUrl,pluginType,outTime){
-            dropifiQuery('#cheetahWidget2012_contactForm').html(""); 
-            var requestUrl = window.location.href; 
-            var transport = new easyXDM.Socket(/** The configuration */{  
-                remote: "http://appengine.dropifi.com/clientswidgets/show_widget_content?publicKey="+apikey+"&buttonPosition="+buttonPosition+"&hostUrl="+hostUrl+"&pluginType="+pluginType+"&requestUrl="+requestUrl, 
+
+
+            var widgetObject = {
+
+                //outer javascript
+                cheetahWidget2012_IframeHeight: function (apikey,buttonPosition,hostUrl,pluginType,outTime){
+                    $('#cheetahWidget2012_contactForm').html("");
+                    var requestUrl = window.location.href;
+                    var transport = new easyXDM.Socket(/** The configuration */{
+                        remote: "http://appengine.dropifi.com/clientswidgets/show_widget_content?publicKey="+apikey+"&buttonPosition="+buttonPosition+"&hostUrl="+hostUrl+"&pluginType="+pluginType+"&requestUrl="+requestUrl,
             //swf: "https://s3.amazonaws.com/dropifi/js/widget/easyxdm.swf",
-            container: "cheetahWidget2012_contactForm",  
-            onMessage: function(message,origin){  
+            container: "cheetahWidget2012_contactForm",
+            onMessage: function(message,origin){
 
-                var data = DropifiWidgetLoad.toArrayJSon(message);  
-                switch(data.type){ 
-                    case 'iframe': 
-                    var h = new Number(data.height);
-                    var iframe = this.container.getElementsByTagName("iframe")[0];                  
-                    iframe.id ='cheetahWidget2012_contactable_iframe';  
-                    iframe.frameBorder = 0; 
-                    iframe.scrolling = 'no';  
+                var data = DropifiWidgetLoad.toArrayJSon(message);
+                switch(data.type){
+                    case 'iframe':
+                    // var h = new Number(data.height);
+
+                    var h = parseInt(data.height,10);
+
+                    var iframe = this.container.getElementsByTagName("iframe")[0];
+                    iframe.id ='cheetahWidget2012_contactable_iframe';
+                    iframe.frameBorder = 0;
+                    iframe.scrolling = 'no';
                     iframe.marginheight="0px";
-                    iframe.marginwidth="0px";           
+                    iframe.marginwidth="0px";
                     iframe.style.height = "100%";
                     iframe.style.width = "100%";
-                    iframe.style.overflow="hidden";                             
-                    iframe.style.border="0px;";             
-                    dropifiQuery("#dropifiwidget_iframe").css({'padding':'0px','margin':'0px','overflow':'hidden','border':'0px'});
+                    iframe.style.overflow="hidden";
+                    iframe.style.border="0px;";
+                    $("#dropifiwidget_iframe").css({'padding':'0px','margin':'0px','overflow':'hidden','border':'0px'});
                     
-                    dropifiQuery('.widget-container #widget-form').css({'height':h+"px"});
+                    $('.widget-container #widget-form').css({'height':h+"px"});
                     DropifiWidgetLoad.resetCFPosition();
                     
-                    break; 
-                    case 'closeWindow':                
-                    DropifiWidgetLoad.dropifi_widget_reload_content = true;
-                    setTimeout('DropifiWidgetLoad.auto_hide_drofpifi_widget()',3500); 
                     break;
+                    case 'closeWindow': DropifiWidgetLoad.dropifi_widget_reload_content = true;
+                        setTimeout('DropifiWidgetLoad.auto_hide_drofpifi_widget()',3500);
+                    break;
+
                     case 'changeHeight':
-                    var h= new Number(data.height);
-                    dropifiQuery('#dropifi_widget_v1_contactable #cheetahWidget2012_contactForm').css({'height':h+"px"});
+                    // var h= new Number(data.height);
+                      h = parseInt(data.height,10);
+
+                    $('#dropifi_widget_v1_contactable #cheetahWidget2012_contactForm').css({'height':h+"px"});
                     if((DropifiWidgetLoad.posi=="right") || (DropifiWidgetLoad.posi=="left")){
-                        DropifiWidgetLoad.resetCFPosition(); 
-                    } 
+                        DropifiWidgetLoad.resetCFPosition();
+                    }
                     
                     break;
                     case 'closeInstantly':
@@ -48,30 +58,43 @@
                         //DropifiWidgetLoad.dropifi_widget_is_close_2012 == false; 
                         DropifiWidgetLoad.auto_hide_drofpifi_widget();
                         break;
-                        case 'captureScreen': 
+                        case 'captureScreen':
                         DropifiWidgetLoad.captureScreen(data.publickey);
-                        break; 
-                    } 
-                    
+                        break;
+                    }
                 },
                 onHeightChange: function(message,origin){
 
                 }
-            });  
-        } ,
-        auto_hide_drofpifi_widget:function (){
-            var this_id_prefix = '#dropifi_widget_v1_contactable'+" "; 
-            if(DropifiWidgetLoad.dropifi_widget_is_close_2012 !=undefined && DropifiWidgetLoad.dropifi_widget_is_close_2012 == false){      
-                dropifiQuery(this_id_prefix+'div#cheetahWidget2012_contactable_inner').trigger('click');
-            }
-        },
-        toArrayJSon: function(message){
-            var arr={};
-            dropifiQuery.each(message.split(','),function(key,val){  
-                var values = val.split(':');         
-                arr[values[0]] = values[1];                 
             });
-            return arr;
-        }
+} ,
+auto_hide_drofpifi_widget:function (){
+    var this_id_prefix = '#dropifi_widget_v1_contactable'+" ";
+    if(DropifiWidgetLoad.dropifi_widget_is_close_2012 !== undefined && DropifiWidgetLoad.dropifi_widget_is_close_2012 === false){
+        $(this_id_prefix+'div#cheetahWidget2012_contactable_inner').trigger('click');
+    }
+},
+toArrayJSon: function(message){
+    var arr={};
+    $.each(message.split(','),function(key,val){
+        var values = val.split(':');
+        arr[values[0]] = values[1];
     });
+    return arr;
+}
+
+
+};
+
+console.log(widgetObject.cheetahWidget2012_IframeHeight('31f362ca9dd8c3a7b9d75fff18ec1615-1406286386228','top','http://www.google.com','pluginType','outTime'));
+
+});
+
+
 })();
+
+
+// })();
+
+
+// <!-- start Dropifi --> <script type='text/javascript' src='//s3.amazonaws.com/dropifi/js/widget/dropifi_widget.min.js'></script><script type='text/javascript'>document.renderDropifiWidget('31f362ca9dd8c3a7b9d75fff18ec1615-1406286386228');</script> <!-- end Dropifi -->
